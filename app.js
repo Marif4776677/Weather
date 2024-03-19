fbcdfae37ef2fc0ac40560f022a72d5fc7142825
Marifdemo4 Ismayilov <marifismayilovv+demo4@gmail.com>
2024-03-19 19:55:41 +0000

    done

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 "use strict";
 $(document).ready(function () {
-
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
+            let a = function (b) {
+                for (let i = 0; i < b; i++) {
+                    $('.box').append(`<h2>${b}</h2>`);
+                }
+            };
+            let x = function (y) {
+                if (Number(y)) {
+                    return a(y);
+                }
+                else {
+                    $('.box').append(`<h2>${y}</h2>`);
+                }
+            };
+            x($('input').val());
+        }
+        $('input').val('');
+    });
 });
