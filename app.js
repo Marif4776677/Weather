21fa7c6c6b1cdd439909fd8bb1386e270e7c3b9f
Marifdemo4 Ismayilov <marifismayilovv+demo4@gmail.com>
2024-03-20 12:54:58 +0000

    done

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-"use strict";
 $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
-            let a = function (b) {
+            let a = b => {
                 for (let i = 0; i < b; i++) {
                     $('.box').append(`<h2>${b}</h2>`);
                 }
             };
-            let x = function (y) {
+            let num = y => {
                 if (Number(y)) {
                     return a(y);
                 }
@@ -16,7 +15,7 @@ $(document).ready(function () {
                     $('.box').append(`<h2>${y}</h2>`);
                 }
             };
-            x($('input').val());
+            num($('input').val());
         }
         $('input').val('');
     });
