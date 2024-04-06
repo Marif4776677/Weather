bc3f1048096365fd5a428a48b1c022cdbc701ad0
Marif Ismayilov <marifismayilovv2+demo@gmail.com>
2024-04-06 08:25:32 +0000

    done

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
+"use strict";
 $(document).ready(function () {
-    $('.btn').click(function (e) {
-        e.preventDefault();
-        if (!$.trim($('input').val()) == '') {
-            let a = b => {
-                for (let i = 0; i < b; i++) {
-                    $('.box').append(`<h2>${b}</h2>`);
-                }
-            };
-            let num = y => {
-                if (Number(y)) {
-                    return a(y);
-                }
-                else {
-                    $('.box').append(`<h2>${y}</h2>`);
-                }
-            };
-            num($('input').val());
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=4196983d205ec806b2ac15e4ef5753e5&units=metric`;
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myweather").val("");
+                console.log(data)
+            }
+            myweather()
         }
-        $('input').val('');
     });
+
+    $("#btn").click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=4196983d205ec806b2ac15e4ef5753e5&units=metric`;
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myweather").val("");
+            console.log(data)
+        }
+        myweather()
+
+    });
+
 });
